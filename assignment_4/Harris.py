3bd32fe20bc0e3254b4aa624bd53bea659c5ee14
Oskar Juell <oskarj@uia.no>
2025-11-02 20:47:59 +0100

    made functions

--- assignment_4/Harris.py
+++ assignment_4/Harris.py
@@ -1,19 +1,24 @@
 import cv2
 import numpy as np
 
-filename = 'chessboard.jpg'
+
+
+
+def harris_corners(reference_image):
+    gray = cv2.cvtColor(reference_image,cv2.COLOR_BGR2GRAY)
+
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray,2,3,0.04)
+
+    #result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst,None)
+
+    # Threshold for an optimal value, it may vary depending on the image.
+    reference_image[dst>0.01*dst.max()]=[0,0,255]
+
+    cv2.imwrite('assignment_4/Harris_corners.png', reference_image)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+
 img = cv2.imread("assignment_4/reference_img.png")
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,2,3,0.04)
-
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst,None)
-
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[0,0,255]
-
-cv2.imwrite('assignment_4/Harris_corners.png', img)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+harris_corners(img)
--- assignment_4/Sift.py
+++ assignment_4/Sift.py
@@ -2,62 +2,67 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+
+
+img1 = cv.imread('assignment_4/align_this.jpg', cv.IMREAD_GRAYSCALE)          # queryImage
+img2 = cv.imread('assignment_4/reference_img.png', cv.IMREAD_GRAYSCALE) # trainImage
 MIN_MATCH_COUNT = 10
 #MAX_FEATURES = 10 did not work assumed you meant min match count here
 GOOD_MATCH_PERCENT = 0.7
 
-img1 = cv.imread('assignment_4/align_this.jpg', cv.IMREAD_GRAYSCALE)          # queryImage
-img2 = cv.imread('assignment_4/reference_img.png', cv.IMREAD_GRAYSCALE) # trainImage
+def sift_align(image_to_align, reference_image, min_match_count=MIN_MATCH_COUNT, good_match_percent=GOOD_MATCH_PERCENT):
+    # Initiate SIFT detector
+    sift = cv.SIFT_create()
 
-# Initiate SIFT detector
-sift = cv.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(image_to_align, None)
+    kp2, des2 = sift.detectAndCompute(reference_image, None)
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
 
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=50)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
 
-flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
 
-matches = flann.knnMatch(des1, des2, k=2)
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m, n in matches:
+        if m.distance < good_match_percent * n.distance:
+            good.append(m)
 
-# store all the good matches as per Lowe's ratio test.
-good = []
-for m, n in matches:
-    if m.distance < GOOD_MATCH_PERCENT * n.distance:
-        good.append(m)
-
-if len(good) > MIN_MATCH_COUNT:
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
- 
-    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-    matchesMask = mask.ravel().tolist()
- 
-    h, w = img1.shape
-    h2, w2 = img2.shape
+    if len(good) > min_match_count:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
     
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+    
+        h, w = image_to_align.shape
+        h2, w2 = reference_image.shape
+        
 
-    img1_aligned = cv.warpPerspective(img1, M, (w2, h2))
+        img1_aligned = cv.warpPerspective(image_to_align, M, (w2, h2))
 
-    pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
-    dst = cv.perspectiveTransform(pts, M)
- 
-    img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
-    cv.imwrite('assignment_4/SIFT_aligned.png', img1_aligned) 
- 
-else:
-    print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
-    matchesMask = None
+        pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+        dst = cv.perspectiveTransform(pts, M)
+    
+        reference_image = cv.polylines(reference_image, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+        cv.imwrite('assignment_4/SIFT_aligned.png', img1_aligned) 
+    
+    else:
+        print("Not enough matches are found - {}/{}".format(len(good), min_match_count))
+        matchesMask = None
 
-draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                   singlePointColor=None,
-                   matchesMask=matchesMask,  # draw only inliers
-                   flags=2)
- 
-img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
- 
-cv.imwrite('assignment_4/SIFT_output.png', img3)
+    draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                    singlePointColor=None,
+                    matchesMask=matchesMask,  # draw only inliers
+                    flags=2)
+    
+    img3 = cv.drawMatches(image_to_align, kp1, reference_image, kp2, good, None, **draw_params)
+    
+    cv.imwrite('assignment_4/SIFT_output.png', img3)
+
+
+sift_align(img1, img2, min_match_count=MIN_MATCH_COUNT, good_match_percent=GOOD_MATCH_PERCENT)
